cbad031fcf1b4e7e3463cbd9d51f9b164fc8463b
victorxup <victor.chuprakov@gmail.com>
2017-12-11 04:51:25 +0300

    add Note dao, service

--- src/main/java/com/epam/lab/spring/khokhliatskii/evernote/dao/NoteDao.java
+++ src/main/java/com/epam/lab/spring/khokhliatskii/evernote/dao/NoteDao.java
@@ -2,10 +2,15 @@ package com.epam.lab.spring.khokhliatskii.evernote.dao;
 
 import com.epam.lab.spring.khokhliatskii.evernote.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoteDao extends JpaRepository<Note, Integer> {
 
+    @Nullable
+    Note findOneByName(String name);
 
+    @Nullable
+    Note findOne(int id);
 }
--- src/main/java/com/epam/lab/spring/khokhliatskii/evernote/service/api/NoteService.java
+++ src/main/java/com/epam/lab/spring/khokhliatskii/evernote/service/api/NoteService.java
@@ -1,4 +1,22 @@
 package com.epam.lab.spring.khokhliatskii.evernote.service.api;
 
+import com.epam.lab.spring.khokhliatskii.evernote.model.Note;
+
+import java.util.List;
+
 public interface NoteService {
+
+    Note get(String name);
+
+    List<Note> getAll();
+
+    Note save(Note note);
+
+    void delete(Note note);
+
+    Note findById(int id);
+
+    boolean isExists(String email);
+
+
 }
