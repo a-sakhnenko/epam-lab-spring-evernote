721799358c2fc3b5ee8e449151f9818e965b51a6
Victor_Chuprakov <victor_chuprakov@epam.com>
2017-12-22 11:50:42 +0300

    add some controllers, update api-map

--- src/main/java/com/epam/lab/spring/khokhliatskii/evernote/controllers/NoteController.java
+++ src/main/java/com/epam/lab/spring/khokhliatskii/evernote/controllers/NoteController.java
@@ -1,47 +1,92 @@
 package com.epam.lab.spring.khokhliatskii.evernote.controllers;
 
 import com.epam.lab.spring.khokhliatskii.evernote.model.Note;
+import com.epam.lab.spring.khokhliatskii.evernote.model.Notebook;
 import com.epam.lab.spring.khokhliatskii.evernote.service.api.NoteService;
+import com.epam.lab.spring.khokhliatskii.evernote.service.api.NotebookService;
+import com.epam.lab.spring.khokhliatskii.evernote.service.api.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.ValidationException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping(value = "/notes")
+@RequestMapping
 public class NoteController {
 
     private static final int FIRST_USER_ID = 1;
 
     @Autowired
     private NoteService noteService;
+    @Autowired
+    private NotebookService notebookService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String getNotebooks(Model model) {
+    @RequestMapping(value = "/notes", method = RequestMethod.GET)
+    public String getNotes(Model model) {
         List<Note> notes = noteService.getAll();
-        model.addAttribute("notes", notes);
+        String email = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+        //get all notes of current user
+        notes = notes.stream()
+                .filter(n -> n.getNotebook().getUser().getEmail().equals(email))
+                .collect(Collectors.toList());
 
+        model.addAttribute("notes", notes);
         return "notes";
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-    public String getNotebook(Model model, @PathVariable(name = "id") int id) {
+    @RequestMapping(method = RequestMethod.GET, value = "/notes/{id}")
+    public String getNote(Model model, @PathVariable(name = "id") int id) {
+        String email = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
         Note note = noteService.findById(id);
+        if(!note.getNotebook().getUser().getEmail().equals(email)){
+            throw (new RuntimeException("Access violation exception"));
+        }
         model.addAttribute("note", note);
 
         return "note";
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String saveNotebook(Model model) {
+    @RequestMapping(value = "/notebooks/{id}",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public @ResponseBody
+    ResponseEntity<String> saveNote(@RequestBody @Valid Note note,
+                                    BindingResult bindingResult,
+                                    @PathVariable(name = "id") int id) {
+        //get logged in username
+        String email = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+        Notebook notebook = notebookService.findById(id);
+        if(!notebook.getUser().getEmail().equals(email)){
+            throw (new RuntimeException("Access violation exception"));
+        }
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Notebook has " + bindingResult.getFieldErrorCount() + " validation errors");
+        }
 
-        return "redirect:/notes";
+        note.setNotebook(notebook);
+        noteService.save(note);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
-
+    //TODO: pass dtos to controller methods, not entities
 }
--- src/main/java/com/epam/lab/spring/khokhliatskii/evernote/controllers/NotebookController.java
+++ src/main/java/com/epam/lab/spring/khokhliatskii/evernote/controllers/NotebookController.java
@@ -8,17 +8,16 @@ import com.epam.lab.spring.khokhliatskii.evernote.service.api.UserService;
 import com.epam.lab.spring.khokhliatskii.evernote.transformer.NotebookTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.ValidationException;
 import java.util.List;
 
@@ -45,20 +44,84 @@ public class NotebookController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public String getNotebook(Model model, @PathVariable(name = "id") int id) {
+        //get logged in username
+        String email = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
         Notebook notebook = notebookService.findById(id);
+        if (!notebook.getUser().getEmail().equals(email)){
+            throw (new RuntimeException("Access violation exception"));
+        }
         model.addAttribute("notebook", notebook);
-
         return "notebook";
     }
 
-    //FIXME: rewrite as POST
-    @RequestMapping(method = RequestMethod.GET, value = "/new/{name}")
-    public String saveNotebook(Model model, @PathVariable(name = "name") String name) {
-        Notebook notebook = new Notebook();
-        notebook.setName(name);
-        notebook.setUser(notebookService.getAll().get(0).getUser());
-//        notebook.setUser(USER_FROM_SECURITY);
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public @ResponseBody
+    ResponseEntity<String>  saveNotebook(@RequestBody @Valid Notebook notebook, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Notebook has " + bindingResult.getFieldErrorCount() + " validation errors");
+        }
+        //get logged in username
+        String email = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+        notebook.setUser(userService.get(email));
         notebookService.save(notebook);
-        return "redirect:/notebooks";
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public @ResponseBody
+    ResponseEntity<String> updateNotebook(@RequestBody @Valid Notebook notebook,
+                                          @PathVariable(name = "id") int id,
+                                          BindingResult bindingResult) {
+
+        String email = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+        Notebook notebookOld = notebookService.findById(id);
+        if (!notebook.getUser().getEmail().equals(email)){
+            throw (new RuntimeException("Access violation exception"));
+        }
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Notebook has " + bindingResult.getFieldErrorCount() + " validation errors");
+        }
+
+        notebookOld.setNotes(notebook.getNotes());
+        notebookOld.setName(notebook.getName());
+        notebookService.save(notebookOld);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public @ResponseBody
+    ResponseEntity<String> deleteNotebook(@PathVariable(name = "id") int id,
+                                          BindingResult bindingResult) {
+        String email = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+        Notebook notebook = notebookService.findById(id);
+        if (!notebook.getUser().getEmail().equals(email)){
+            throw (new RuntimeException("Access violation exception"));
+        }
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Notebook has " + bindingResult.getFieldErrorCount() + " validation errors");
+        }
+        notebookService.delete(notebook);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    //TODO: move logic to service as much as possible, make @transactional
+
+
+
 }
